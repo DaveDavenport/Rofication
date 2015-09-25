349460f05e440715ae6967117fb7ce0046598e02
QC <qball@gmpclient.org>
2015-09-25 21:34:02 +0200

    Make a class.

--- rofication-daemon.py
+++ rofication-daemon.py
@@ -15,8 +15,7 @@ import json
 from msg import Msg,Urgency
 
 event = threading.Event()
-notification_queue = []
-notification_queue_lock = threading.Lock()
+
 
 """
     This is a list of applications where only the last notification is relevant.
@@ -30,85 +29,133 @@ single_notification_app=[ "VLC media player" ]
 allowed_expire_app=[ ]
 
 
-"""
-    This function updates the queue. E.g. removes popups that are expired and allowed to expire
-"""
-def update_queue():
-    with notification_queue_lock:
-        now = time.time()
-        n = [ n for n in notification_queue if n.application in allowed_expire_app and n.deadline > 0 and n.deadline < now ];
-        for no in n:
-            print("{mid} expired.".format(mid=no.mid))
-            notification_queue.remove(no)
+class Rofication(threading.Thread):
+    
+    def __init__(self):
+        self.notification_queue = []
+        self.notification_queue_lock = threading.Lock()
+        self.last_id=0
+        super().__init__()
 
-def remove_notification(id):
-    printf("Removing: {}".format(id))
-    with notification_queue_lock:
-        n = [ n for n in notification_queue_lock if n.notid == id ]
-        for no in n:
-            print("Closing: {id}:{sum}".format(id=no.mid, sum=no.application))
-
-def add_notification(notif):
-    with notification_queue_lock:
-        if notif.application in  single_notification_app:
-            n = [ n for n in notification_queue if n.application == notif.application ];
-            for no in n:
-                notification_queue.remove(no)
-
-        notification_queue.append(notif)
-
-def message_thread(dummy):
-    server = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    server.bind("/tmp/rofi_notification_daemon")
-    server.listen(1)
-    server.settimeout(1)
-    while 1:
+    def load(self):
+        print("Loading rofication")
         try:
-            connection, client_address = server.accept()
-            update_queue()
-            try:
-                data = connection.recv(4)
-                data = data.decode('utf-8')
-                if data == "numn":
-                    with notification_queue_lock:
-                        u = [ n for n in notification_queue if n.urgency == Urgency.critical ];
-                        mstr = "{lent}\n{ul}".format(lent=len(notification_queue),ul=str(len(u)))
-                        connection.send(bytes(mstr, 'utf-8'))
-                if data == "list":
-                    with notification_queue_lock:
-                        i=0
-                        for noti in notification_queue:
-                            connection.send(bytes(jsonpickle.encode(noti),'utf-8'))
-                            connection.send(b'\n')
-                            i+=1
-                # Dismiss and item.
-                elif data == "del:":
-                    data = connection.recv(4).decode('utf-8')
-                    print("Delete: {0}".format(data))
-                    for noti in notification_queue:
-                        if noti.mid == int(data):
-                            notification_queue.remove(noti)
-                            break
-                # Saw an item, this sets the urgency to normal.
-                elif data == "saw:":
-                    data = connection.recv(4).decode('utf-8')
-                    for noti in notification_queue:
-                        if noti.mid == int(data):
-                            noti.urgency = int(Urgency.normal)
-            finally:
-                # Clean up the connection
-                connection.close()
+            with open('not.json', 'r') as f:
+                    self.notification_queue = jsonpickle.decode(f.read())
         except:
-            if event.is_set():
-                break;
-    server.close()
-    os.unlink("/tmp/rofi_notification_daemon")
+            pass
+
+        for noti in self.notification_queue:
+            noti.notid=-1
+            if self.last_id < noti.mid:
+                self.last_id = int(noti.mid)
+        print("Found last id: {nid}".format(nid=nf._id))
+
+    def save(self):
+        print("Saving rofication")
+        try:
+            print(len(self.notification_queue))
+            with open('not.json','w') as f:
+                f.write(jsonpickle.encode(self.notification_queue))
+        except:
+            print("Failed to store queue.")
+    """
+        This function updates the queue. E.g. removes popups that are expired and allowed to expire
+    """
+    def update_queue(self):
+        with self.notification_queue_lock:
+            now = time.time()
+            n = [ n for n in self.notification_queue if n.application in allowed_expire_app and n.deadline > 0 and n.deadline < now ];
+            for no in n:
+                print("{mid} expired.".format(mid=no.mid))
+                self.notification_queue.remove(no)
+
+    def remove_notification(self,id):
+        printf("Removing: {}".format(id))
+        with self.notification_queue_lock:
+            n = [ n for n in self.notification_queue_lock if n.notid == id ]
+            for no in n:
+                print("Closing: {id}:{sum}".format(id=no.mid, sum=no.application))
+
+    def add_notification(self,notif):
+        with self.notification_queue_lock:
+            if notif.application in  single_notification_app:
+                n = [ n for n in self.notification_queue if n.application == notif.application ];
+                for no in n:
+                    self.notification_queue.remove(no)
+
+            self.notification_queue.append(notif)
+
+    """
+        Communication command.
+    """
+    def communication_command_send_list(self,connection):
+        with self.notification_queue_lock:
+            i=0
+            for noti in self.notification_queue:
+                connection.send(bytes(jsonpickle.encode(noti),'utf-8'))
+                connection.send(b'\n')
+                i+=1
+    def communication_command_delete(self, connection, arg): 
+        with self.notification_queue_lock:
+            for noti in self.notification_queue:
+                if noti.mid == int(arg):
+                    self.notification_queue.remove(noti)
+                    break
+
+    def communication_command_saw(self, connection, arg):
+        with self.notification_queue_lock:
+            for noti in self.notification_queue:
+                if noti.mid == int(arg):
+                    noti.urgency = int(Urgency.normal)
+                    break
+
+    def communication_command_num(self, connection):
+        with self.notification_queue_lock:
+            u = [ n for n in self.notification_queue if n.urgency == Urgency.critical ];
+            mstr = "{lent}\n{ul}".format(lent=len(self.notification_queue),ul=str(len(u)))
+            connection.send(bytes(mstr, 'utf-8'))
+
+    def run(self):
+        server = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        server.bind("/tmp/rofi_notification_daemon")
+        server.listen(1)
+        server.settimeout(1)
+        while 1:
+            try:
+                connection, client_address = server.accept()
+                self.update_queue()
+                try:
+                    data = connection.recv(1024).decode('utf-8')
+                    command = data.split(':')[0]
+
+                    # Get number of notifications
+                    if command == "num":
+                        self.communication_command_num(connection)
+                    # Getting a listing.
+                    elif command == "list":
+                        self.communication_command_send_list(connection)
+                    # Dismiss and item.
+                    elif command == "del":
+                        self.communication_command_delete(connection,data.split(':')[1])
+                    # Saw an item, this sets the urgency to normal.
+                    elif command == "saw":
+                        self.communication_command_saw(connection, data.split(':')[1])
+                finally:
+                    # Clean up the connection
+                    connection.close()
+            except:
+                if event.is_set():
+                    break;
+        server.close()
+        os.unlink("/tmp/rofi_notification_daemon")
 
 """
     DBUS Notification Listener and Fetcher.
 """
 class NotificationFetcher(dbus.service.Object):
     _id = 0
+    _rofication = None
 
     @dbus.service.method("org.freedesktop.Notifications",
                          in_signature='susssasa{ss}i',
@@ -127,7 +174,7 @@ class NotificationFetcher(dbus.service.Object):
             msg.deadline= time.time()+int(expire_timeout) / 1000.0
         if 'urgency' in hints:
             msg.urgency = int(hints['urgency'])
-        add_notification( msg )
+        self._rofication.add_notification( msg )
         return notification_id
 
     @dbus.service.method("org.freedesktop.Notifications", in_signature='', out_signature='as')
@@ -136,38 +183,38 @@ class NotificationFetcher(dbus.service.Object):
 
     @dbus.service.signal('org.freedesktop.Notifications', signature='uu')
     def NotificationClosed(self, id_in, reason_in):
-        remove_notification(id_in)
+        _rofication.remove_notification(id_in)
         pass
 
     @dbus.service.method("org.freedesktop.Notifications", in_signature='u', out_signature='')
     def CloseNotification(self, id):
-        remove_notification(id)
+        _rofication.remove_notification(id)
         pass
 
     @dbus.service.method("org.freedesktop.Notifications", in_signature='', out_signature='ssss')
     def GetServerInformation(self):
       return ("rofication", "http://gmpclient.org/", "0.0.1", "1")
 
+"""
+    Main function
+"""
 if __name__ == '__main__':
+    """ Create daemon """
+    rofication = Rofication();
+
+    """ Setup DBUS"""
     dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
     session_bus = dbus.SessionBus()
     name = dbus.service.BusName("org.freedesktop.Notifications", session_bus)
     nf = NotificationFetcher(session_bus, '/org/freedesktop/Notifications')
 
-    try:
-        with open('not.json', 'r') as f:
-            notification_queue = jsonpickle.decode(f.read())
-    except:
-        pass
+    nf._rofication = rofication;
+    
+    rofication.load();
+    nf._id = rofication.last_id
 
-    for noti in notification_queue:
-        noti.notid=-1
-        if nf._id < noti.mid:
-            nf._id = int(noti.mid)
-    print("Found last id: {nid}".format(nid=nf._id))
     # Thread handling sockets.
-    t1 = threading.Thread(target=message_thread, args=[None])
-    t1.start()
+    rofication.start()
     ##
     # Create Glib mainloop, this is needed to make dbus work.
     # GMainLoop also takes care for catching signals.
@@ -178,9 +225,5 @@ if __name__ == '__main__':
         # Set event telling it to quit.
         event.set()
     # Join unix socket thread.
-    t1.join()
-    try:
-        with open('not.json','w') as f:
-            f.write(jsonpickle.encode(notification_queue))
-    except:
-        print("Failed to store queue.")
+    rofication.join()
+    rofication.save()
--- rofication-statusi3blocks.py
+++ rofication-statusi3blocks.py
@@ -3,7 +3,7 @@ import sys
 import socket
 client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 client.connect("/tmp/rofi_notification_daemon")
-client.send(b"numn",4)
+client.sendall(bytes("num",'utf-8'))
 
 val = client.recv(32)
 val = val.decode('utf-8')
