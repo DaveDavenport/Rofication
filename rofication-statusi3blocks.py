c526eb20322ad0cbd100c2ea84546f092d41fadb
Ken Gilmer <kgilmer@gmail.com>
2019-11-29 21:47:52 -0800

    Bug fixes to blocket.

--- rofication-statusi3blocks.py
+++ rofication-statusi3blocks.py
@@ -1,13 +1,43 @@
 #!/usr/bin/env python3
 import sys
 import socket
-client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-client.connect("/tmp/rofi_notification_daemon")
-client.sendall(bytes("num",'utf-8'))
+import os
+from subprocess import check_output, run
 
-val = client.recv(32)
-val = val.decode('utf-8')
-l = val.split('\n',2)
-print(str(l[0]))
-if int(l[1]) > 0:
+notification_empty = os.getenv('i3xrocks_label_notify_none', check_output(['/usr/bin/xrescat', 'i3xrocks.label.notify.none', 'N'], universal_newlines=True))
+notification_some = os.getenv('i3xrocks_label_notify_some', check_output(['/usr/bin/xrescat', 'i3xrocks.label.notify.some', 'N'], universal_newlines=True))
+notification_error = os.getenv('i3xrocks_label_notify_error', check_output(['/usr/bin/xrescat', 'i3xrocks.label.notify.error', 'N'], universal_newlines=True))
+default_color = os.getenv('color', check_output(['/usr/bin/xrescat', 'i3xrocks.value.color', '#D8DEE9'], universal_newlines=True))
+default_label_color = os.getenv('label_color', check_output(['/usr/bin/xrescat', 'i3xrocks.label.color', '#7B8394'], universal_newlines=True))
+valuefont = os.getenv('font', check_output(['/usr/bin/xrescat', 'i3xrocks.value.font', 'Source Code Pro Medium 13'], universal_newlines=True))
+
+notification_value = 0
+icon = notification_empty
+has_error = False
+form = '<span color="{}">{}</span><span font_desc="{}" color="{}"> {}</span>'
+
+try:
+    client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    client.connect("/tmp/rofi_notification_daemon")
+    client.sendall(bytes("num",'utf-8'))
+
+    val = client.recv(32)
+    val = val.decode('utf-8')
+    l = val.split('\n',2)
+    notification_value = int(l[0])
+    if (notification_value > 0):
+        icon = notification_some
+    if int(l[1]) > 0:
+        icon = notification_error
+        has_error = True
+except (FileNotFoundError, ConnectionRefusedError):
+    icon = notification_error
+    has_error = True
+
+print (form.format(default_label_color, icon, valuefont, default_color, notification_value))
+
+if (has_error): 
     exit(33)
+
+if (os.getenv('button', "")):
+    run(['/usr/bin/python3', '/tmp/Rofication/rofication-gui.py'])
