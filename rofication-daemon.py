cc67521e460acfca5b624dee24c6cf64be17c258
Marco Buzzanca <marco.bzn@gmail.com>
2020-04-02 19:26:02 +0200

    Use serversocket instead of plain sockets

--- rofication-daemon.py
+++ rofication-daemon.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import os
 
 import dbus
 from dbus.mainloop import glib
@@ -7,21 +6,14 @@ from gi.repository import GLib
 
 import handler
 import notification
-import rofication
+from rofication import NotificationServer
 
 """
     Main function
 """
 if __name__ == '__main__':
-    """ Pre-start cleanup """
-    if os.path.exists("/tmp/rofi_notification_daemon"):
-        os.remove("/tmp/rofi_notification_daemon")
-
     """ Create notification queue """
     not_queue = notification.NotificationQueue.load("not.json")
-
-    """ Create daemon """
-    rofication = rofication.Rofication(not_queue)
 
     """ Setup DBUS"""
     glib.DBusGMainLoop(set_as_default=True)
@@ -29,17 +21,15 @@ if __name__ == '__main__':
     bus_name = dbus.service.BusName("org.freedesktop.Notifications", session_bus)
     not_handler = handler.NotificationHandler(session_bus, '/org/freedesktop/Notifications', not_queue)
 
-    # Thread handling sockets.
-    rofication.start()
-    ##
-    # Create Glib mainloop, this is needed to make dbus work.
-    # GMainLoop also takes care for catching signals.
-    ##
-    try:
-        GLib.MainLoop().run()
-    except:
-        # Set event telling it to quit.
-        rofication.event.set()
-    # Join unix socket thread.
-    rofication.join()
+    with NotificationServer("/tmp/rofi_notification_daemon", not_queue) as server:
+        server.start()
+        try:
+            ##
+            # Create Glib mainloop, this is needed to make dbus work.
+            # GMainLoop also takes care for catching signals.
+            ##
+            GLib.MainLoop().run()
+        except:
+            server.shutdown()
+
     not_queue.save("not.json")
--- rofication.py
+++ rofication.py
@@ -1,93 +1,85 @@
 import os
-import socket
 import threading
+from socketserver import ThreadingMixIn, UnixStreamServer, BaseRequestHandler
 
 import jsonpickle
 
-from notification import Urgency, NotificationQueue
+from notification import NotificationQueue, Urgency
 
 
-class Rofication(threading.Thread):
-    def __init__(self, queue: NotificationQueue) -> None:
-        super().__init__()
-        self.socket_path: str = "/tmp/rofi_notification_daemon"
-        self.nq: NotificationQueue = queue
-        self.event: threading.Event = threading.Event()
-
-    """
-        Communication command.
-    """
-
-    def communication_command_send_list(self, connection: socket) -> None:
-        with self.nq.lock:
-            for notification in self.nq.queue:
-                connection.send(bytes(jsonpickle.encode(notification), 'utf-8'))
-                connection.send(b'\n')
+class NotificationHandler(BaseRequestHandler):
+    def communication_command_send_list(self) -> None:
+        with self.server.not_queue.lock:
+            for notification in self.server.not_queue.queue:
+                self.request.send(bytes(jsonpickle.encode(notification), 'utf-8'))
+                self.request.send(b'\n')
 
     def communication_command_delete(self, nid: int) -> None:
-        with self.nq.lock:
-            self.nq.remove(nid)
+        with self.server.not_queue.lock:
+            self.server.not_queue.remove(nid)
 
     def communication_command_delete_apps(self, application: str) -> None:
-        with self.nq.lock:
-            to_remove = [n.id for n in self.nq.queue
+        with self.server.not_queue.lock:
+            to_remove = [n.id for n in self.server.not_queue.queue
                          if n.application == application]
-            self.nq.remove_all(to_remove)
+            self.server.not_queue.remove_all(to_remove)
 
     def communication_command_saw(self, nid: int) -> None:
-        with self.nq.lock:
-            self.nq.see(nid)
+        with self.server.not_queue.lock:
+            self.server.not_queue.see(nid)
 
-    def communication_command_num(self, connection: socket) -> None:
-        with self.nq.lock:
+    def communication_command_num(self) -> None:
+        with self.server.not_queue.lock:
             urgent = 0
-            for n in self.nq.queue:
+            for n in self.server.not_queue.queue:
                 if n.urgency == Urgency.CRITICAL:
                     n += 1
-            cmd = "{:d}\n{:d}".format(len(self.nq.queue), urgent)
-            connection.send(bytes(cmd, 'utf-8'))
+            cmd = "{:d}\n{:d}".format(len(self.server.not_queue.queue), urgent)
+            self.request.send(bytes(cmd, 'utf-8'))
 
-    def run(self) -> None:
-        server = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        try:
-            server.bind(self.socket_path)
-            server.listen(1)
-            server.settimeout(1)
-        except OSError as e:
-            print("Failed to open socket: " + str(e))
-            exit(1)
-        while True:
-            try:
-                connection, client_address = server.accept()
-                with self.nq.lock:
-                    self.nq.cleanup()
-                try:
-                    data = connection.recv(1024).decode('utf-8')
-                    command = data.split(':')[0]
+    def handle(self) -> None:
+        with self.server.not_queue.lock:
+            self.server.not_queue.cleanup()
 
-                    # Get number of notifications
-                    if command == "num":
-                        self.communication_command_num(connection)
-                    # Getting a listing.
-                    elif command == "list":
-                        self.communication_command_send_list(connection)
-                    # Dismiss and item.
-                    elif command == "del":
-                        nid = int(data.split(':')[1])
-                        self.communication_command_delete(nid)
-                    elif command == "dela":
-                        application = data.split(':')[1]
-                        self.communication_command_delete_apps(application)
-                    # Saw an item, this sets the urgency to normal.
-                    elif command == "saw":
-                        nid = int(data.split(':')[1])
-                        self.communication_command_saw(nid)
-                finally:
-                    # Clean up the connection
-                    connection.close()
-            except:
-                if self.event.is_set():
-                    break
+        data = self.request.recv(1024).decode('utf-8').strip()
+        command = data.split(':')[0]
 
-        server.close()
-        os.unlink("/tmp/rofi_notification_daemon")
+        # Get number of notifications
+        if command == "num":
+            self.communication_command_num()
+        # Getting a listing.
+        elif command == "list":
+            self.communication_command_send_list()
+        # Dismiss and item.
+        elif command == "del":
+            nid = int(data.split(':')[1])
+            self.communication_command_delete(nid)
+        elif command == "dela":
+            application = data.split(':')[1]
+            self.communication_command_delete_apps(application)
+        # Saw an item, this sets the urgency to normal.
+        elif command == "saw":
+            nid = int(data.split(':')[1])
+            self.communication_command_saw(nid)
+
+
+class ThreadedUnixStreamServer(ThreadingMixIn, UnixStreamServer):
+    def start(self) -> threading.Thread:
+        thread = threading.Thread(target=self.serve_forever)
+        thread.setDaemon(True)
+        thread.start()
+        return thread
+
+
+class NotificationServer(ThreadedUnixStreamServer):
+    def __init__(self, server_address: str, not_queue: NotificationQueue) -> None:
+        # Pre-start cleanup
+        if os.path.exists(server_address):
+            os.remove(server_address)
+        super().__init__(server_address, NotificationHandler)
+        self.not_queue: NotificationQueue = not_queue
+
+    def __exit__(self, *args) -> None:
+        super().__exit__(*args)
+        # removes the UNIX socket after use
+        os.remove(self.server_address)
